e46249eadc3000368c210541727cc3cc04854d6c
Daniil0309 <ch-do@mail.ru>
2024-09-07 18:24:42 +0700

    new DJ02 DZ
    new DJ02 DZ

--- zerocoder/main/urls.py
+++ zerocoder/main/urls.py
@@ -2,7 +2,14 @@
 from django.urls import path
 from .import views
 
+# urlpatterns = [
+#     path('', views.index, name='home'),  # Главная страница
+#     path('new', views.new, name='page2'),  # Вторая страница
+# ]
+
 urlpatterns = [
-    path('', views.index, name='home'),  # Главная страница
-    path('new', views.new, name='page2'),  # Вторая страница
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('services/', views.services, name='services'),  # Новый маршрут для "Услуги"
 ]
--- zerocoder/main/views.py
+++ zerocoder/main/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-def index(request):
-    return render(request, 'main/index.html', {'caption': 'CatDjango'})
-def new(request):
-    return render(request, 'main/new.html')
+# def index(request):
+#     return render(request, 'main/index.html', {'caption': 'CatDjango'})
+# def new(request):
+#     return render(request, 'main/new.html')
+
+
+def home(request):
+    return render(request, 'main/index.html')
+
+def services(request):
+    return render(request, 'main/services.html')  # Новый view для страницы "Услуги"
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def contact(request):
+    return render(request, 'main/contact.html')
